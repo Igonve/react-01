ca7d56bc25e7df4aefaf3159cf4e5ce4b9d62b43
Igonve <63926649+Igonve@users.noreply.github.com>
2021-03-21 22:21:11 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+
+import Header from "./components/header/header.component";
+import HomePage from "./pages/homepage/homepage.component";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  // ciclos de vida: montado, actualizado y un desmontado
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+          console.log(this.state);
+        });
+      }
+      this.setState({ currentUser: userAuth });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
